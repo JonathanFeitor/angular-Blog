f8ff30c95c7b38f416d964ca1c975e8b0981c9e8
Jonathan Feitor <jonathan.feitor@hotmail.com>
2023-06-10 15:01:13 -0300

    created a new page and added dynamism between them. Also added a file to feed this new page.

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,58 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "aaa",
+        "title": "Joker is the greatest of villains",
+        "description": "DC announces the new Joker movie and promises even more villains in the film. According to the film's producer, this will be the most violent joker of all Batman ever created.",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/03/coringa_jared_leto__p2z38fcoj-1210x544.jpg"
+    },
+    {
+        "id": "2",
+        "photo": "aaa",
+        "title": "new Cyborg movie",
+        "description": "DC launches the new Cyborg movie, a character that appears in the last Justice League movie. The producer promises a lot of action and violence, thus being prohibited for children under 16 years old.",
+        "photoCover": "https://images6.alphacoders.com/868/thumb-1920-868447.jpg"
+    },
+    {
+        "id": "3",
+        "photo": "aaa",
+        "title": "DC series will join",
+        "description": "DC will unite the characters of Green Arrow, Flash and Supergirl, leaving even more viewers waiting for the new seasons of the series.",
+        "photoCover": "https://mixdeseries.com.br/wp-content/uploads/2023/03/the-flash-arrow-produtor-perda-de-tempo.webp"
+    },
+    {
+        "id": "4",
+        "photo": "aaa",
+        "title": "Black Adam promises to change the course of movies",
+        "description": "Black Adam, the new DC movie hopes to raise billions at the box office. Even as DC's strongest character, Black Adam is neither super hero nor villain, he still seeks redemption.",
+        "photoCover": "https://epipoca.com.br/wp-content/uploads/2021/08/adao-negro-1200x900-1.jpg"
+    },
+    {
+        "id": "5",
+        "photo": "aaa",
+        "title": "The Suicide Squad Director and Producer Take Over",
+        "description": "Suicide Squad gets a new director, above a relaxed photo with Harley Quinn character from the new squad movie.",
+        "photoCover": "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2022/10/25/1914300654-james-gunn-suicide-squad-set.jpg"
+    },
+    {
+        "id": "6",
+        "photo": "aaa",
+        "title": "justice league much anticipated movie",
+        "description": "Here comes a new justice league movie, according to the DC director, it will be a long movie, telling the story of the new characters that will be integrated into the league.",
+        "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2022/10/hamada-dc.jpg"
+    },
+    {
+        "id": "7",
+        "photo": "aaa",
+        "title": "Green Lantern new movie",
+        "description": "Green Lantern movie hopes to attract many new fans, as it will show how this DC character faces the villains of different planets, saving millions of people.",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLxCtN8g6nRVKRRNTNZUlcTLYKqofEgnzpnw&usqp=CAU"
+    },
+    {
+        "id": "8",
+        "photo": "aaa",
+        "title": "new Batman movie",
+        "description": "Here comes the new Batman, informed by DC that this new film will be even bloodier, and will contain some villains that did not appear in the last films of the character. Much more violence and action is expected, being prohibited for children under 16 years old.",
+        "photoCover": "https://frenezirevista.files.wordpress.com/2022/03/batman-capa-2-1.jpg"
+    },
+]
